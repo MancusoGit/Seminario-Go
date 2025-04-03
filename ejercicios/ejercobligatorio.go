836328c6cf557afccf63eb99a202512d27ad1855
MancusoGit <totomancus145@gmail.com>
2025-04-03 12:03:16 -0300

    primer version ej obligatorio

---
+++ ejercicios/ejercobligatorio.go
@@ -0,0 +1,30 @@
+package ejercicios
+
+import (
+	"strings"
+)
+
+func ImparReverb(palabra string) string {
+	// Esta funcion recibe una palabra y devuelve la misma palabra pero con las letras en orden inverso
+
+	palabras := strings.Fields(palabra) // convierte la palabra en un slice de palabras
+	var resultado strings.Builder       // crea un buffer de strings
+	for i := 0; i < len(palabras); i++ {
+		if i%2 == 0 { // si es par
+			revertirPalabra(palabras[i], &resultado)
+		} else {
+			resultado.WriteString(palabras[i]) // agrega la palabra al resultado
+		}
+		if i < len(palabras)-1 { // si no es la ultima palabra
+			resultado.WriteString(" ") // agrega un espacio
+		}
+	}
+	return resultado.String() // retorna el resultado
+}
+
+func revertirPalabra(palabra string, resultado *strings.Builder) {
+	// Esta funcion recibe una palabra y la agrega al resultado pero en orden inverso
+	for i := len(palabra) - 1; i >= 0; i-- { // recorre la palabra al reves
+		resultado.WriteByte(palabra[i]) // agrega la letra al resultado
+	}
+}
